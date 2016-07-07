d14d9e84fa0f69e8bf41e40e44635933152bdf3e
Evgeny Cherkashin <eugene@irnok.net>
2016-07-07 11:25:11 +0800

    Poppler made XML parser implementation

---
+++ src/icc/studprogs/popplerxml.py
@@ -0,0 +1,19 @@
+from common import *
+
+class Loader(object):
+    """Loads poppler xml, translates it via iterators.
+    """
+
+    def __init__(self, file=stream, encoding=):
+        """
+
+        Arguments:
+        - `file`:
+        """
+        self._file = file
+
+
+
+
+if __name__=="__main__":
+    main(STREAM)
