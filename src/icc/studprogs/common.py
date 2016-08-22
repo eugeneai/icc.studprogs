03d935c78f49ca8c0ec8cf92cc2f8bfd77b16127
Evgeny Cherkashin <eugene@irnok.net>
2016-08-23 07:18:46 +0800

    Beautified source.

--- src/icc/studprogs/common.py
+++ src/icc/studprogs/common.py
@@ -1,24 +1,26 @@
 import icc.studprogs.uctotokenizer as ucto
 
+
 class Symbol(object):
     """Class of marker instances
     """
     individuals = {}
+
     def __init__(self, name, mark=None, nospace=False, **kwargs):
         """
         """
-        self.name=name
-        self._mark=mark
-        self._nospace=nospace
+        self.name = name
+        self._mark = mark
+        self._nospace = nospace
         for k, v in kwargs.items():
-            setattr(self, k,v)
+            setattr(self, k, v)
 
     def __str__(self, ):
         """
         """
         return "<Symbol {}>".format(self.name)
 
-    __repr__=__str__
+    __repr__ = __str__
 
     def nospace(self):
         return self._nospace()
@@ -26,27 +28,29 @@ class Symbol(object):
     def mark(self, style=None):
         """Print it in a markdown like mode.
         """
-        if style=="hidden":
+        if style == "hidden":
             return ""
         if style in [None, "shown"]:
-            if self._mark != None:
+            if self._mark is not None:
                 return self._mark
             else:
                 return str(self)
 
-page_symbol=Symbol("page",r"\newpage"+"\n", nospace=True)
-paragraph_symbol=Symbol("paragraph", r"\par"+"\n", nospace=True)
-line_tab=Symbol("line tab", r"-->", nospace=True)
-line_tail=Symbol("line tail", r"<--", nospace=True)
-line_start=Symbol("line start", "", nospace=True)
-line_end=Symbol("line end", "\n", nospace=True)
-sentence_end=Symbol("sentence end", "\n", nospace=True)
-symbol_anchor_start=Symbol("anchor", "<", nospace=True)
-symbol_anchor_end=Symbol("anchor", ">", nospace=False)
+
+page_symbol = Symbol("page", r"\newpage" + "\n", nospace=True)
+paragraph_symbol = Symbol("paragraph", r"\par" + "\n", nospace=True)
+line_tab = Symbol("line tab", r"-->", nospace=True)
+line_tail = Symbol("line tail", r"<--", nospace=True)
+line_start = Symbol("line start", "", nospace=True)
+line_end = Symbol("line end", "\n", nospace=True)
+sentence_end = Symbol("sentence end", "\n", nospace=True)
+symbol_anchor_start = Symbol("anchor", "<", nospace=True)
+symbol_anchor_end = Symbol("anchor", ">", nospace=False)
 font_bold_start = Symbol("start bold", "**", nospace=True)
 font_bold_end = Symbol("end bold", "**", nospace=False)
 font_italic_start = Symbol("start italic", "*", nospace=True)
 font_italic_end = Symbol("end italic", "*", nospace=False)
+
 
 class BaseLoader(object):
     """Implements basic loader functions.
@@ -59,8 +63,8 @@ class BaseLoader(object):
         - `file`: string filename or open [binary] file to be loaded.
         - `encoding`: encoding of input text defaults to utf-8.
         """
-        if type(file)==type(""):
-            file=open(file,"rb")
+        if type(file) == type(""):
+            file = open(file, "rb")
         self.file = file
         self.encoding = encoding
         self.options = kwargs
@@ -71,8 +75,7 @@ class BaseLoader(object):
         self.options.setdefault("line_paragraph", True)
         self.options.setdefault("empty_line_paragraph", False)
         self.tokenizer = ucto.Tokenizer(sentencedetection=True)
-        self._skip=0
-
+        self._skip = 0
 
     def initialize(self):
         """Initializes internal structures of
@@ -84,7 +87,7 @@ class BaseLoader(object):
         """Skips amount of lines.
         Useful for debugging
         """
-        self._skip=amount
+        self._skip = amount
 
     def lines(self):
         """Generates lines and page/paragraph events
@@ -101,8 +104,8 @@ class BaseLoader(object):
                     yield sent
                     sent = []
                 else:
-                    #if sent:
-                    #    raise RuntimeError("symbol inside a sentence")
+                    # if sent:
+                    #     raise RuntimeError("symbol inside a sentence")
                     if sent:
                         yield sent
                         sent = []
@@ -113,41 +116,42 @@ class BaseLoader(object):
             yield sent
 
     def paragraphs(self,
-                   pages_are_paragraphs = True,
+                   pages_are_paragraphs=True,
                    join=False,
                    style=None,
-                   decorations=("",""),
+                   decorations=("", ""),
                    only_words=False,
-                   by_sentences=False
-    ):
+                   by_sentences=False):
         if join:
-            for par in self.paragraphs(pages_are_paragraphs = pages_are_paragraphs,
-                                join=False):
+            for par in self.paragraphs(
+                    pages_are_paragraphs=pages_are_paragraphs, join=False):
                 npar = []
                 for lexem in par:
-                    space=" "
+                    space = " "
                     if type(lexem) == tuple:
                         token, _style = lexem
                         try:
-                            tok_type=token.type()
+                            tok_type = token.type()
                         except AttributeError:
-                            tok_type="WORD"
+                            tok_type = "WORD"
                         if only_words:
-                            if not tok_type in ["WORD", "PUNCTUATION-MULTI", "PUNCTUATION"]:
+                            if tok_type not in ["WORD", "PUNCTUATION-MULTI",
+                                                "PUNCTUATION"]:
                                 continue
                             if tok_type in ["PUNCTUATION-MULTI"]:
-                                #space=""
-                                token="."
-                            t9=str(token)
+                                # space=""
+                                token = "."
+                            t9 = str(token)
                             if tok_type == "WORD" and not t9.isalpha():
                                 continue
                     else:
                         token = lexem.mark(style=style)
-                        #if not token:
-                            #space=""
-                    #if hasattr(token, "nospace") and token.nospace():
-                        #space=""
-                    npar.append(decorations[0]+str(token)+decorations[1]+space)
+                        # if not token:
+                    # space=""
+                    # if hasattr(token, "nospace") and token.nospace():
+                    # space=""
+                    npar.append(decorations[0] + str(token) + decorations[1] +
+                                space)
                 yield "".join(npar)
             return
         # Generate paragraph as list of lexems
@@ -169,27 +173,27 @@ class BaseLoader(object):
             yield paragraph
 
     def lexems(self, from_line=None, style=None):
-        tokenizer=self.tokenizer
-        if style==None:
-            style={}
-        if from_line!=None:
-            src=[from_line]
+        tokenizer = self.tokenizer
+        if style is None:
+            style = {}
+        if from_line is not None:
+            src = [from_line]
         else:
-            src=self.lines()
+            src = self.lines()
 
         for line in src:
-            if type(line)==type(""):
-                any_tok=False
+            if type(line) == type(""):
+                any_tok = False
                 tokenizer.process(line)
                 for token in tokenizer.tokens():
                     any_tok = True
                     yield token, style
-                    if str(token)=="Программное":
+                    if str(token) == "Программное":
                         pass
                     if token.isendofsentence():
                         yield sentence_end
-                    #if token.isnewparagraph():
-                    #    pass
+                    # if token.isnewparagraph():
+                    #     pass
                 if self.options["line_paragraph"]:
                     yield paragraph_symbol
 
