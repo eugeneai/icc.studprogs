9733df3e71ed629d2690865d7b16aa1594bf56b7
Evgeny Cherkashin (at mux.irnok.net) <eugeneai@irnok.net>
2016-09-19 16:27:55 +0800

    Added recursive processing of documents.

--- src/icc/tests/test_learner.py
+++ src/icc/tests/test_learner.py
@@ -7,18 +7,28 @@ from nose.plugins.skip import SkipTest
 from glob import glob
 import os.path
 
-NO_LONG_DOCS = False
+LONG_DOCS = True
 
 package = __name__
 DATA_DIR = resource_filename("icc.studprogs", "data/annotations/")
 DOC_DIR = os.path.join(DATA_DIR, "documents")
 EXT_PATTERN = "*.docx"
+DOC_EXT_PATTERN = "**/"+EXT_PATTERN
 LEARN_PATTERN = "*-learn.xml"
 
 FILES = glob(os.path.join(DATA_DIR, EXT_PATTERN))
-DOCS = glob(os.path.join(DOC_DIR, EXT_PATTERN))
+if LONG_DOCS:
+    g = os.path.join(DOC_DIR, DOC_EXT_PATTERN)
+    DOCS = glob(g, recursive=True)
+    #print (DOCS)
+    #print ("glob:", g)
+
 LEARN_FILES = glob(os.path.join(DATA_DIR, LEARN_PATTERN))
 
+def absoluteFilePaths(directory):
+   for dirpath,_,filenames in os.walk(directory):
+       for f in filenames:
+           yield os.path.abspath(os.path.join(dirpath, f))
 
 class TestBasicLoad:
     def setUp(self):
@@ -75,7 +85,7 @@ class TestLearning:
     #     # print("Declinations:", recon - y)
 
     def test_predict_on_annotations(self):
-        self.e.fit(debug=True)
+        self.e.fit(debug=False)
         for docx_file in FILES:
             xml = XMLTextPropertyExtractor(
                 filename=docx_file, importer=msdocx.Importer)
@@ -93,22 +103,51 @@ class TestLearning:
             assert ny is not None
 
     def test_predict_on_documens(self):
-        global NO_LONG_DOCS
-        if NO_LONG_DOCS:
+        global LONG_DOCS
+        if not LONG_DOCS:
             print ("Long documents skipped")
             return
+        else:
+            print ("Processing long documents")
 
         self.e.fit()
-        for docx_file in DOCS:
-            xml = XMLTextPropertyExtractor(
-                filename=docx_file, importer=msdocx.Importer)
-            output_filename = docx_file.replace("annotations/documents","out").replace(".docx","-document-extracted.xml")
-            xml.load()
+        ldocs=len(DOCS)
+        for i, docx_file in enumerate(DOCS):
+            #if i!=219:
+            #    continue
+            print("{} of {}: {}".format(i+1, ldocs, docx_file))
+            fp,lp=docx_file.split("annotations/documents/")
+            lp.strip(".docx")
+            lp=lp.replace(" ","_")
+            lp=lp.replace("/","__")
+            fn=os.path.join(fp,"out",lp)
+            extracted_filename = fn+"-document-extracted.xml"
+            predicted_filename = fn+"-document-predicted.xml"
+            print(predicted_filename)
+
+            try:
+                os.stat(predicted_filename)
+                print ("Already done.")
+                continue
+            except OSError:
+                pass
+
+            xml = XMLTextPropertyExtractor(filename=docx_file,
+                                           importer=msdocx.Importer)
+            try:
+                xml.load()
+            except etree.XMLSyntaxError:
+                print ("Cannot load xml.")
+                continue
             xml.extract()
-            xml.write(output_filename)
+            # print("OUT:",output_filename)
+            try:
+                xml.write(extracted_filename)
+            except OSError as e:
+                print("Cannot write:", extracted_filename, e)
+                continue
             xml.set_learn_coding(self.e.learn_coding)
             nx = xml.prepare_params()
             ny = self.e.predict(extractor=xml)
-            output_filename = docx_file.replace("annotations/documents","out").replace(".docx","-document-predicted.xml")
-            xml.write(output_filename)
+            xml.write(predicted_filename)
             assert ny is not None
