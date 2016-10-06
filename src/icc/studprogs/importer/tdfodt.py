d54cd9b8e308be6fc11087aca7073552a0de608d
Evgeny Cherkashin (at mux.irnok.net) <eugeneai@irnok.net>
2016-10-06 17:05:52 +0800

    Processed most of ODF elements.

--- src/icc/studprogs/importer/tdfodt.py
+++ src/icc/studprogs/importer/tdfodt.py
@@ -47,14 +47,17 @@ class Importer(BaseImporter):
 
     def body(self, node, root):
         for e, t, a in self.iterchildren(node):
-            if t=="office:text":
-                self.body(e, root)
             if t in {"text:tracked-changes","text:sequence-decls"}:
                 continue
-            if t == "text:p":
+            if t=="office:text":
+                self.body(e, root)
+            elif t == "text:p":
                 self.p(e, root)
             elif t=="text:list":
-                self.list(e,root)
+                list_=etree.SubElement(root,"list")
+                self.list(e,list_)
+            elif t=="table:table":
+                self.table(e,root)
             else:
                 print("body:", e.tagName, a)
 
@@ -71,16 +74,61 @@ class Importer(BaseImporter):
                     if tt.nodeType == element.Node.TEXT_NODE:
                         text+=tt.data
                 sty.text=text
+            elif t=="text:a":
+                sty=etree.SubElement(par, "style")
+                sty.set("id", a[('urn:oasis:names:tc:opendocument:xmlns:text:1.0', 'style-name')])
+                URL=a[('http://www.w3.org/1999/xlink', 'href')]
+                sty.text=URL
             else:
                 print ("par:", t, a)
 
     def list(self, node, root):
         for e, t, a in self.iterchildren(node):
             if t=="text:list-item":
-                self.list_item(e, root, node)
+                li=etree.SubElement(root,"li")
+                self.list_item(e, li, node)
             else:
                 print ("list:", t,a)
 
     def list_item(self, node, root, list_node):
         for e, t, a in self.iterchildren(node):
-            print ("list-item:", t,a)
+            if t=="text:p":
+                self.p(e,root)
+            else:
+                print ("list-item:", t,a)
+
+    def table(self, node, root):
+        table=etree.SubElement(root,"table")
+        row=0
+        for e, t, a in self.iterchildren(node):
+            if t=="table:table-column":
+                self.table_column(e,table)
+            elif t=="table:table-row":
+                self.table_row(e,table,row)
+                row+=1
+            else:
+                print("table:",t,a)
+
+    def table_column(self, node, root):
+        for e, t, a in self.iterchildren(node):
+            print ("table-column:", t,a)
+
+    def table_row(self, node, root, row):
+        col=0
+        for e, t, a in self.iterchildren(node):
+            if t=="table:table-cell":
+                span_rows=a.get(('urn:oasis:names:tc:opendocument:xmlns:table:1.0', 'number-rows-spanned'),'1')
+                span_cols=a.get(('urn:oasis:names:tc:opendocument:xmlns:table:1.0', 'number-cols-spanned'),'1')
+                cell=etree.SubElement(root, 'cell')
+                cell.set("x",str(col))
+                cell.set("y",str(row))
+                cell.set("w",span_cols)
+                cell.set("h",span_rows)
+                cell.get("p","-1")
+                self.body(e,cell)
+            else:
+                print ("table-row:", t,a)
+
+    def cell(self, node, root):
+        for e, t, a in self.iterchildren(node):
+            print("cell:",t,a)
